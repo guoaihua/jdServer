52fe549ceecdd6f3ce9e4c77385b20700c1f2e5d
guoaihua <guoaihua@geetest.com>
2018-05-02 09:05:30 +0800

    添加session

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session = require('express-session');
+var cors = require('cors');
+
 
 var app = express();
 
@@ -18,9 +20,21 @@ app.set('view engine', 'jade');
 app.all('*',function (req,res,next) {
 	res.header("Access-Control-Allow-Origin","*");
   res.header("Access-Control-Allow-Headers","Content-Type");
+    res.header("Access-Control-Allow-Credentials",true);
 	next();
 });
 
+app.use(cookieParser('aa'));
+
+// 创建session
+app.use(session({
+    secret: 'aa',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 80000 , secure: false }
+}))
+
+app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +42,9 @@ app.use(cookieParser());
 app.use(express.static('public'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var bodyparser = require('body-parser');
+var URL = 'http://localhost:3001';
+var app = express();
 
-  var URL = 'http://localhost:3000'
+/*使用bodyparder中间件*/
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('*', function(req, res, next) {
+	if(req.session) {
+        next();
+	}else{
+		res.send({status: "fail"});
+	}
 });
 
+router.get('/login', function (req, res) {
+		var data = JSON.parse(req.query.form);
+    	if(!req.session.username){
+            req.session.username = data.user;
+            console.log(req.session);
+            res.json({ret_code: 0, ret_msg: '登录成功',ret_name:req.session.username});
+		}else{
+            console.log(req.session);
+            res.json({ret_code: 0, ret_msg: '登录成功'});
+		}
+
+})
 router.get('/slide_imgs', function(req, res, next){
-  var imgs = [
-  	    {src: URL+'/images/slide_imgs/bg1.jpg'},
-  	   	{src: URL+'/images/slide_imgs/bg1.jpg'},
-  	   	{src: URL+'/images/slide_imgs/bg1.jpg'},
-  ]
-  res.send(imgs)
+	console.log(req.session);
+	if(req.session.username){
+		console.log(11111);
+        var imgs = [
+            {src: URL+'/images/slide_imgs/bg1.jpg'},
+            {src: URL+'/images/slide_imgs/bg1.jpg'},
+            {src: URL+'/images/slide_imgs/bg1.jpg'}
+        ]
+        res.send(imgs)
+	}else {
+		res.send({status: 0})
+	}
+
 })
 
 router.get('/list_imgs', function(req, res, next){
-  var URL = 'http://localhost:3000'
-
-
 
   var items = [
   			{ banner: URL+'/images/pics.jpg',
@@ -44,7 +72,59 @@ router.get('/list_imgs', function(req, res, next){
 
 router.get('/class_names', function (req, res, next) {
 	// body...
-	var names = ["春装","春装","春装","春装","春装","春装","春装","春装","春装","春装","春装","春装","春装","春装"]
+	var names = [
+		{
+			"name": "春装",
+			"id": "0"
+		},
+				{
+			"name": "春装",
+			"id": "1"
+		},
+			{
+			"name": "春装",
+			"id": "2"  
+		},
+				{
+			"name": "春装",
+			"id": "3"
+		},
+				{
+			"name": "春装",
+			"id": "4"
+		},
+			{
+			"name": "春装",
+			"id": "5"  
+		},
+				{
+			"name": "春装",
+			"id": "6"
+		},
+				{
+			"name": "春装",
+			"id": "7"
+		},
+			{
+			"name": "春装",
+			"id": "8"  
+		},			{
+			"name": "春装",
+			"id": "9"  
+		},
+				{
+			"name": "春装",
+			"id": "10"
+		},
+				{
+			"name": "春装",
+			"id": "11"
+		},
+			{
+			"name": "春装",
+			"id": "12"  
+		}
+	]
 	res.send(names)
 })
 module.exports = router;
